527f6d2ab381b0607a3c5a16c40aed500ead5241
Samuel Morrissey <samuel.morrissey@outlook.com>
2023-01-11 21:25:21 -0300

    Limpeza inicial e primeiros modelos

---
+++ src/model/Todo.tsx
@@ -0,0 +1,9 @@
+// A Todo entity interface
+
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
---
+++ src/model/User.tsx
@@ -0,0 +1,9 @@
+// A interface that describes the User
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
